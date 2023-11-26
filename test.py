722ccc1362d0e0d117c56a84d89d54b95c412c10
anhdng8@gmail.com <anhdng8@gmail.com>
2023-11-26 00:35:48 -0500

    Nov 26

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import gymnasium as gym
 from stable_baselines3.ppo import PPO
 import os
 import argparse
+import datetime
+import time
 
 #constants
 model_dir = "model"
@@ -11,25 +13,29 @@ os.makedirs(log_dir, exist_ok = True)
 ENV = 'HalfCheetah-v4'
 
 def train():
-    model = PPO('MlpPolicy', ENV, verbose=1, device="cuda", tensorboard_log=log_dir) #MlpNN #change hyperParams
+    model = PPO('MlpPolicy', ENV, verbose=1, device="cuda", tensorboard_log=log_dir, learning_rate=0.0002, gamma=0.97, n_steps=512) #MlpNN #change hyperParams
     TIMESTEPS = 1000000
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
-    model.save(f"{model_dir}/PPO_{TIMESTEPS}")
+    
+    # Generate a timestamp in the "PPO_Jan18_1830PM" format
+    current_time = datetime.datetime.now().strftime("%b%d_%I%M%p")
+    
+    # Append the formatted timestamp to the model filename
+    model_filename = f"{model_dir}/PPO_{current_time}_{TIMESTEPS}timesteps"
+    model.save(model_filename)
 
 def test(path_to_model):
     env = gym.make(ENV, render_mode = 'human')
     model = PPO.load(path_to_model, env = env)
     obs, _ = env.reset()
     done = False
-    extra_steps = 500
+
     while True:
         action, _ = model.predict(obs) #passing state to get action
         obs, _, done, _, _ = env.step(action) #pass action to get next state
-        
+        time.sleep(0.001)
         if done:
-            extra_steps -= 1
-            if extra_steps < 0:
-                break
+            break
     
 if __name__ == '__main__':
     #parse cmd inputs
